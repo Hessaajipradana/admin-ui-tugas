320590522b7e578799b076f15c15803e8b8a2484
maqilfahmi <111202214001@mhs.dinus.ac.id>
2024-10-14 17:42:51 +0700

    14 oktober

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-function App() { 
-  return (
-	  <>
-	    <h1 className="text-3xl font-bold underline">
-	      Hello world!
-	    </h1>
-      <h1 className="text-3xl font-bold text-purple-600">
-	      Halo Bandung
-	    </h1>
-      <h1 className="text-3xl font-bold underline">
-	      Hello Bromo
-	    </h1>
-	  </>
-  )
-}
-
-
-export default App
+const App = () => {
+  return(
+<>
+</>
+  );
+};
+  
+  export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,28 @@ export default {
   ],
   theme: {
     extend: {
-      
+      colors: {
+        primary: "#299D91",
+        secondary: "#525256",
+        defaultBlack: "#191919", 
+        gray: {
+          "01": "#666666",
+          "02": "#878787",
+          "03": "#9F9F9F", 
+          "05": "#E8E8E8",
+          "06": "#F3F3F3",
+        },
+        special: {
+          "bg": "rgba(210, 210, 210, 0.25)",
+          "bg2": "rgba(255, 255, 255, 0.7)",
+          "bg3": "rgba(255, 255, 255, 0.08)",
+          "mainBg": "#F4F5F7",
+          "green": "#E73D1C",
+          "red": "#4DAF6E"
+        } 
+      },
       fontFamily: {
-        body: ['Edu Australia VIC WA NT Hand Guides']
+        body: ['Noto Serif Oriya']
       }
     },
   },
